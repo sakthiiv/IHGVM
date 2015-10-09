c13d4cc36b125019024854596d3a4addf02f45d5
Sakthivel Viswanathan <visvel@gmail.com>
2015-10-09 17:47:23 +0530

    Update Locator.cs

--- IHGVM_VideoSplitter/Locator.cs
+++ IHGVM_VideoSplitter/Locator.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace IHGVM_VideoSplitter
 {
