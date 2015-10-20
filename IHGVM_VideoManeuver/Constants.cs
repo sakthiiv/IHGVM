cd8a3c664ecf76e5c6b059671e48152c367f4b64
Sakthivel Viswanathan <visvel@gmail.com>
2015-10-20 12:30:20 +0530

    Update Constants.cs

--- IHGVM_VideoManeuver/Constants.cs
+++ IHGVM_VideoManeuver/Constants.cs
@@ -10,12 +10,17 @@ namespace IHGVM_VideoManeuver
         public const string LABLE_CODEC = "Codec Type of this Video: ";
         public const string STRING_PATH = @"..\BitMaps\";
         public const string FILE_FILTERS = "Avi files (*.avi)|*.avi|All files (*.*)|*.*";
+        public const string APPLICATION_CAPTION = "IHGVM";
+        public const string SAVEALL_ERROR = "No new videos processed. Please choose a video to process.";
+        public const string SAVE_ERROR = "Please choose an activity to save.";
     }
 
     public class AVI32_CONSTANTS
     {
         public static int PALETTE_SIZE = 4 * 256;
+        public const int CONS_WRITE = 1;
         public const int CONS_READWRITE = 2;
+        public const int CONS_CREATE = 4096;
         public const int BMP_MAGIC_COOKIE = 19778;
     }
 }
